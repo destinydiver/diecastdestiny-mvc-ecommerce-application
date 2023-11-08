b20b0144524509677f04bee2897c9c18e8454799
Chuck <destinydiver@gmail.com>
2023-11-08 11:18:09 -0500

    Add project files.

---
+++ DiecastDestiny/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DiecastDestiny.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
