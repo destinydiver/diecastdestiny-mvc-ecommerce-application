651c2496217e614c97fa38c56bcb280d32c2bf52
Chuck <destinydiver@gmail.com>
2023-12-08 14:03:01 -0500

    IdentityAdded

--- DiecastDestiny/Migrations/20231208185653_fourthIM.cs
+++ DiecastDestiny/Migrations/20231208185653_fourthIM.cs
@@ -5,7 +5,7 @@
 namespace DiecastDestiny.Migrations
 {
     /// <inheritdoc />
-    public partial class brandFix4 : Migration
+    public partial class fourthIM : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
